e03305c7d7a2401a86bec860d94e5a30ec131e4b
zezim.ricardo <zezim.ricardo@81c103ca-a533-4d0a-5a99-9fe5e4e64535>
2011-11-19 22:06:24 +0000

    Alteração na interface. Colocado mapa para servir de base para todo mundo. Cada um cooque sua api-key do google maps para poder usar localmente.

--- Cliente/src/br/ufc/activity/ClienteActivity.java
+++ Cliente/src/br/ufc/activity/ClienteActivity.java
@@ -1,13 +1,20 @@
 package br.ufc.activity;
 
-import android.app.Activity;
 import android.os.Bundle;
 
-public class ClienteActivity extends Activity {
+import com.google.android.maps.MapActivity;
+
+public class ClienteActivity extends MapActivity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
+
+	@Override
+	protected boolean isRouteDisplayed() {
+		// TODO Auto-generated method stub
+		return false;
+	}
 }
