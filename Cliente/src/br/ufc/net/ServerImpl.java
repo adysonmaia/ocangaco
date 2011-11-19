a525a44811d383fdbb40579b05146b0c6835f4a0
andrefonteles@gmail.com <andrefonteles@gmail.com@81c103ca-a533-4d0a-5a99-9fe5e4e64535>
2011-11-19 18:33:15 +0000

    Criado um exemplo em ServerImpl onde um comando é enviado para o servidor.
    É assim que deve ser feito? Não sei usar a API de comunicação do Danilo.

--- Cliente/src/br/ufc/net/ServerImpl.java
+++ Cliente/src/br/ufc/net/ServerImpl.java
@@ -4,6 +4,10 @@ import br.ufc.model.Player;
 
 public class ServerImpl implements IServer{
 
+	// TODO : Atualizar o SERVER_IP com o IP adequado para teste
+	public static final String SERVER_IP = "localhost";
+	public static final int PORT = 8080;
+	
 	/**
 	 * Construtor com visibilidade de pacote para tornar obrigatória a instanciação desse Server
 	 * por meio de ServerFactory
@@ -24,8 +28,17 @@ public class ServerImpl implements IServer{
 	
 	@Override
 	public void updatePlayersLocation(Player player) {
-		// TODO Auto-generated method stub
+		String comando;
 		
+		comando = "<movimentacao>, " + 
+				player.getNome() + ", " + player.getLatitude() + ", "+ player.getLongitude() + 
+				  ",<movimentacao>";
+		
+		try {
+			Conexao.executaComando(comando, SERVER_IP, PORT);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Override
