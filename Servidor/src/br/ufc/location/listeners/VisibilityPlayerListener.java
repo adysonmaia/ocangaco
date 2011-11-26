e48eba4bda87d9b267024da5ffcda31bb71eabbd
daniloreis10@gmail.com <daniloreis10@gmail.com@81c103ca-a533-4d0a-5a99-9fe5e4e64535>
2011-11-26 20:42:55 +0000

    Listener que resolve se dois jogadores são visiveis

--- Servidor/src/br/ufc/location/listeners/VisibilityPlayerListener.java
+++ Servidor/src/br/ufc/location/listeners/VisibilityPlayerListener.java
@@ -2,11 +2,18 @@ package visibilitylistener;
 
 import facade.IMobileDevice;
 import facade.IVisibilityListener;
+import game.Player;
 
 public class VisibilityPlayerListener implements IVisibilityListener{
 
 	@Override
 	public boolean isVisible(IMobileDevice device1, IMobileDevice device2,double distance) {
+		 // Verifica se o dispositivo é do time adversário
+		 if(device1.getGroup() != device2.getGroup()){
+			 if ((distance > 0)&&(distance < Player.VIEW_DISTANCE)){
+				 return true;
+			 }
+		 }
 		return false;
 	}
 
