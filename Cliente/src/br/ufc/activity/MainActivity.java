63b836d8f3846b00a378252c23ec9eb4eae518ac
adyson.maia@gmail.com <adyson.maia@gmail.com@81c103ca-a533-4d0a-5a99-9fe5e4e64535>
2011-11-28 18:10:38 +0000

    Modificando classe Player para possuir o papel do jogador

--- Cliente/src/br/ufc/activity/MainActivity.java
+++ Cliente/src/br/ufc/activity/MainActivity.java
@@ -6,8 +6,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import br.ufc.model.ClientGameState;
 import br.ufc.model.Player;
 import br.ufc.util.Properties;
@@ -17,7 +19,8 @@ public class MainActivity  extends Activity implements OnClickListener {
 	
 	private EditText editTextIP;
 	private EditText editTextName;
-	private EditText editTextTeam;
+	private Spinner spinnerTeam;
+	private Spinner spinnerRole;
 	private Button button;
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -25,9 +28,18 @@ public class MainActivity  extends Activity implements OnClickListener {
 		setContentView(R.layout.main);
 		
 		editTextName = (EditText)findViewById(R.id.editText1);
-		editTextTeam = (EditText)findViewById(R.id.editText2);
 		editTextIP = (EditText)findViewById(R.id.editText3);
 		editTextIP.setText(Properties.SERVER_IP);
+		
+		spinnerRole = (Spinner)findViewById(R.id.spinnerRole);
+		ArrayAdapter<CharSequence> adapterRole = ArrayAdapter.createFromResource(this, R.array.roles_array, android.R.layout.simple_spinner_item);
+		adapterRole.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		spinnerRole.setAdapter(adapterRole);
+		
+		spinnerTeam = (Spinner)findViewById(R.id.spinnerTeam);
+		ArrayAdapter<CharSequence> adapterTeam = ArrayAdapter.createFromResource(this, R.array.team_array, android.R.layout.simple_spinner_item);
+		adapterTeam.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		spinnerTeam.setAdapter(adapterTeam);
 		
 		button = (Button)findViewById(R.id.button1);
 		button.setOnClickListener(this);
@@ -36,10 +48,12 @@ public class MainActivity  extends Activity implements OnClickListener {
 	@Override
 	public void onClick(View arg0) {
 		Properties.SERVER_IP = editTextIP.getText().toString();
+		int role = spinnerRole.getSelectedItemPosition() +1;
+		int team = spinnerTeam.getSelectedItemPosition() +1;
 
 		ClientGameState.eu = new Player(
 				editTextName.getText().toString(), 
-				Integer.parseInt(editTextTeam.getText().toString()));
+				team, role);
 		
 		Intent i = new Intent(this, ClienteActivity.class);
 		startActivity(i);
