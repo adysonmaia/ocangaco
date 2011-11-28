5962296227a0ce01b75be890972eb208e0e2dba7
adyson.maia@gmail.com <adyson.maia@gmail.com@81c103ca-a533-4d0a-5a99-9fe5e4e64535>
2011-11-28 18:10:38 +0000

    Modificando classe Player para possuir o papel do jogador

--- trunk/Cliente/src/br/ufc/model/Player.java
+++ trunk/Cliente/src/br/ufc/model/Player.java
@@ -2,6 +2,7 @@ package br.ufc.model;
 
 import com.google.android.maps.GeoPoint;
 
+
 /**
  * @author Andr� Fonteles, Rafael de Lima e Benedito Neto
  *
@@ -13,8 +14,14 @@ public class Player {
 	public static final int CANGACEIRO = 1;
 	public static final int JAGUNCO = 2;
 	
+	public static final int MUNICIADOR = 1;
+	public static final int MEDICO     = 2;
+	public static final int ENGENHEIRO = 3;
+	public static final int ESPIAO     = 4;
+	
 	private String nome;
 	private int tipo;
+	private int papel;
 	private double latitude;
 	private double longitude;
 
@@ -22,6 +29,13 @@ public class Player {
 		super();
 		this.nome = nome;
 		this.tipo = tipo;
+	}
+	
+	public Player(String nome, int tipo, int papel) {
+		super();
+		this.nome  = nome;
+		this.tipo  = tipo;
+		this.papel = papel;
 	}
 
 	public Player(String nome) {
@@ -33,6 +47,15 @@ public class Player {
 		super();
 		this.nome = nome;
 		this.tipo = tipo;
+		this.latitude = latitude;
+		this.longitude = longitude;
+	}
+	
+	public Player(String nome, int tipo, int papel, double latitude, double longitude) {
+		super();
+		this.nome = nome;
+		this.tipo = tipo;
+		this.papel = papel;
 		this.latitude = latitude;
 		this.longitude = longitude;
 	}
@@ -71,5 +94,13 @@ public class Player {
 
 	public GeoPoint createLocationGeoPoint() {
 		return new GeoPoint((int)(latitude * 1E6), (int) (longitude * 1E6));
+	}
+
+	public int getPapel() {
+		return papel;
+	}
+
+	public void setPapel(int papel) {
+		this.papel = papel;
 	}
 }
