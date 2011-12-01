f59ffb56e250caf14ed06d4d53937c3fd658f119
danielrmatos@gmail.com <danielrmatos@gmail.com@81c103ca-a533-4d0a-5a99-9fe5e4e64535>
2011-12-01 01:25:56 +0000

    Barricada

---
+++ Servidor/src/br/ufc/location/listeners/ProximityBarricadaListener.java
@@ -0,0 +1,69 @@
+package br.ufc.location.listeners;
+
+import br.ufc.location.facade.IMobileDevice;
+import br.ufc.location.facade.IProximityListener;
+import br.ufc.servidor.artefatos.Mina;
+import br.ufc.servidor.player.Player;
+
+/**
+ * Esta classe implementa a a��o relacionada a uma mina quando um outro 
+ * dispositivo se aproxima
+ * @author Rafael
+ *
+ */
+public class ProximityBarricadaListener implements IProximityListener{
+
+	@Override
+	public void action(IMobileDevice mina, IMobileDevice device2,double distance) {
+		 
+		 // Verifica se o dispositivo � do time advers�rio
+		 if(mina.getGroup() != device2.getGroup()){
+			 if ((distance > Mina.COLISION_DISTANCE)&&(distance < Mina.VIEW_DISTANCE)){
+				 actionVisibleArea(device2);
+			 }
+			 else{
+				 if (distance <Mina.COLISION_DISTANCE){
+					 actionCollisionArea(device2);
+				 }
+			 }
+		 }
+	}
+	/**
+	 * 
+	 * @param device
+	 */
+	private void actionVisibleArea(IMobileDevice device){
+		 // Dar o tratamento para cada tipo de objeto do time inimigo
+		 switch(device.getType()){
+		    // Outro Jogador
+		 case Player.SOLDIER:			 
+		 case Player.DOCTOR:			 
+		 case Player.ENGINEER:			 
+		 case Player.SPY:
+			 System.out.printf("To avistando uma mina inimiga\n");
+			 break;
+		 }
+	}
+	/**
+	 * 
+	 * @param device
+	 */
+	private void actionCollisionArea(IMobileDevice device){
+		 // Dar o tratamento para cada tipo de objeto do time inimigo
+		 switch(device.getType()){
+		 // Outro Jogador
+		 case Player.SOLDIER:
+			 System.out.printf("soldado inimigo atingido por uma mina\n");
+			 break;
+		 case Player.DOCTOR:
+			 System.out.printf("curandeiro inimigo atingido por uma mina\n");
+			 break;
+		 case Player.ENGINEER:
+			 System.out.printf("pedreiro inimigo atingido por uma mina\n");
+			 break;
+		 case Player.SPY:
+			 System.out.printf("dedo duro inimigo atingido por uma mina\n");
+			 break;
+		 }
+	}
+}
--- Servidor/src/br/ufc/servidor/artefatos/Barricada.java
+++ Servidor/src/br/ufc/servidor/artefatos/Barricada.java
@@ -1,8 +1,56 @@
 package br.ufc.servidor.artefatos;
 
-public class Barricada {
-	private double latitude, logintude;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
 
+import br.ufc.location.facade.IGeoPosition;
+import br.ufc.location.facade.IMobileDevice;
+import br.ufc.location.facade.IProximityListener;
+import br.ufc.location.facade.IVisibilityListener;
+import br.ufc.location.geoengine.DevicePath;
+import br.ufc.location.geoengine.DevicesPositionControl;
+import br.ufc.location.geoengine.GeoPosition;
+import br.ufc.location.listeners.ProximityBarricadaListener;
+import br.ufc.location.listeners.VisibilityBarricadaListener;
+import br.ufc.util.XMLParser;
+
+public class Barricada implements IMobileDevice{
+	private double latitude, longitude;
+
+	private Integer id;
+	private GeoPosition position;
+	private double distance;
+	private double distanceOn;
+	private Integer type;
+	private Integer groupId;
+	private DevicePath path;
+	private ProximityBarricadaListener proximity;
+	private VisibilityBarricadaListener visibility;
+	
+	public static final int VIEW_DISTANCE = 100;
+	public static final int COLISION_DISTANCE = 10;
+	
+	public Barricada(int group, double latitude, double longitude) {
+		this.groupId = group;
+		this.latitude = latitude;
+		this.longitude = longitude;
+		
+		proximity = new ProximityBarricadaListener();
+		visibility = new VisibilityBarricadaListener();
+		path = new DevicePath();
+	}
+	
+	public Barricada(){
+		this(1,0,0);
+	}
+	
+	@Override
+	public String toString() {
+		return "Barricada [groupId=" + groupId
+				+ ", latitude=" + latitude + ", longitude=" + longitude + "]";
+	}
+	
 	/**
 	 * @return the latitude
 	 */
@@ -21,13 +69,249 @@ public class Barricada {
 	 * @return the logintude
 	 */
 	public double getLogintude() {
-		return logintude;
+		return longitude;
 	}
 
 	/**
 	 * @param logintude the logintude to set
 	 */
 	public void setLogintude(double logintude) {
-		this.logintude = logintude;
+		this.longitude = logintude;
+	}
+
+	/**
+	 * @return the id
+	 */
+	public Integer getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the position
+	 */
+	public GeoPosition getPosition() {
+		return position;
+	}
+
+	/**
+	 * @param position the position to set
+	 */
+	public void setPosition(GeoPosition position) {
+		this.position = position;
+	}
+
+	/**
+	 * @return the distance
+	 */
+	public double getDistance() {
+		return distance;
+	}
+
+	/**
+	 * @param distance the distance to set
+	 */
+	public void setDistance(double distance) {
+		this.distance = distance;
+	}
+
+	/**
+	 * @return the distanceOn
+	 */
+	public double getDistanceOn() {
+		return distanceOn;
+	}
+
+	/**
+	 * @param distanceOn the distanceOn to set
+	 */
+	public void setDistanceOn(double distanceOn) {
+		this.distanceOn = distanceOn;
+	}
+
+	/**
+	 * @return the type
+	 */
+	public Integer getType() {
+		return type;
+	}
+
+	/**
+	 * @param type the type to set
+	 */
+	public void setType(Integer type) {
+		this.type = type;
+	}
+
+	/**
+	 * @return the groupId
+	 */
+	public Integer getGroupId() {
+		return groupId;
+	}
+
+	/**
+	 * @param groupId the groupId to set
+	 */
+	public void setGroupId(Integer groupId) {
+		this.groupId = groupId;
+	}
+
+	/**
+	 * @return the path
+	 */
+	public DevicePath getPath() {
+		return path;
+	}
+
+	/**
+	 * @param path the path to set
+	 */
+	public void setPath(DevicePath path) {
+		this.path = path;
+	}
+
+	/**
+	 * @return the proximity
+	 */
+	public ProximityBarricadaListener getProximity() {
+		return proximity;
+	}
+
+	/**
+	 * @param proximity the proximity to set
+	 */
+	public void setProximity(ProximityBarricadaListener proximity) {
+		this.proximity = proximity;
+	}
+
+	/**
+	 * @return the visibility
+	 */
+	public VisibilityBarricadaListener getVisibility() {
+		return visibility;
+	}
+
+	/**
+	 * @param visibility the visibility to set
+	 */
+	public void setVisibility(VisibilityBarricadaListener visibility) {
+		this.visibility = visibility;
+	}
+
+	@Override
+	public void setGeoPosition(IGeoPosition position) {
+		this.position = (GeoPosition) position;
+		
+	}
+
+	@Override
+	public IGeoPosition getGeoPosition() {
+		return position;
+	}
+
+	@Override
+	public double getDistanceFrom(IMobileDevice device) {
+		distance = DevicesPositionControl.calculateDistance(this
+				.getGeoPosition(), device.getGeoPosition());
+		return distance;
+	}
+
+	@Override
+	public double getLastDistance() {
+		return distance;
+	}
+
+	@Override
+	public void setGroup(Integer group) {
+		this.groupId = group;
+		
+	}
+
+	@Override
+	public Integer getGroup() {
+		return groupId;
+	}
+
+	@Override
+	public void setDevicePath(DevicePath path) {
+		this.path = path;
+		
+	}
+
+	@Override
+	public DevicePath getDevicePath() {
+		return path;
+	}
+
+	@Override
+	public void setProximityListener(IProximityListener listener) {
+		this.proximity = (ProximityBarricadaListener) listener;
+		
+	}
+
+	@Override
+	public IProximityListener getProximityListener() {
+		return proximity;
+	}
+
+	@Override
+	public IVisibilityListener getVisibilityListener() {
+		return visibility;
+	}
+
+	@Override
+	public String toXML() throws Exception {
+		Document doc = XMLParser.createXMLDocument();
+		if (doc != null) {
+			toXML(null, doc);
+			return XMLParser.getXMLString(doc);
+		} else {
+			throw new Exception("XML parser error");
+		}
+	}
+
+	@Override
+	public void toXML(Element parent, Document doc) {
+		Element element = doc.createElement("mina");
+		if (parent == null) {
+			doc.appendChild(element);
+		} else {
+			parent.appendChild(element);
+		}
+		// set attributes to barricada element		
+		element.setAttribute("grupo", String.valueOf(this.getGroup()));
+		element.setAttribute("latitude", String.valueOf(this.getLatitude()));
+		element.setAttribute("longitude", String.valueOf(this.getLogintude()));
+		
+	}
+	
+	public void fromXML(String xmlString) throws Exception {
+		Document doc = XMLParser.createXMLDocument(xmlString);
+
+		if (doc != null) {
+			NodeList nodes = doc.getElementsByTagName("barricada");
+			Element p = (Element) nodes.item(0);
+
+			if (p != null) {
+				fromXML(p);
+			}
+		} else {
+			throw new Exception("XML parser error");
+		}
+	}
+	
+	public void fromXML(Element element){
+		this.setGroup(Integer.parseInt(element.getAttribute("grupo")));
+		this.setLatitude(Double.parseDouble(element
+						.getAttribute("latitude")));
+		this.setLogintude(Double.parseDouble(element
+				.getAttribute("longitude")));
 	}
 }
