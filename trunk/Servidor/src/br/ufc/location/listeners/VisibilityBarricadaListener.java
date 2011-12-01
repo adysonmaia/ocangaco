97c59b7afdfb331a57642f13a3108f33d3e450fb
danielrmatos@gmail.com <danielrmatos@gmail.com@81c103ca-a533-4d0a-5a99-9fe5e4e64535>
2011-12-01 01:25:56 +0000

    Barricada

---
+++ trunk/Servidor/src/br/ufc/location/listeners/VisibilityBarricadaListener.java
@@ -0,0 +1,25 @@
+package br.ufc.location.listeners;
+
+import br.ufc.location.facade.IMobileDevice;
+import br.ufc.location.facade.IVisibilityListener;
+import br.ufc.servidor.artefatos.Mina;
+
+/**
+ * Esta classe implementa o que ocorre quando a mina entra no campo de visualiza��o de algum
+ * dispositivo
+ * @author Rafael
+ *
+ */
+public class VisibilityBarricadaListener implements IVisibilityListener{
+
+	@Override
+	public boolean isVisible(IMobileDevice device1, IMobileDevice device2,double distance) {
+		 // Verifica se o dispositivo � do time advers�rio
+		 if(device1.getGroup() != device2.getGroup()){
+			 if ((distance > 0)&&(distance < Mina.VIEW_DISTANCE)){
+				 return true;
+			 }
+		 }
+		return false;
+	}
+}
