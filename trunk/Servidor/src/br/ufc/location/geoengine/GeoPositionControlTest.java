3a1cb362381a92fbed4c5f8ee29231354a017845
daniloreis10@gmail.com <daniloreis10@gmail.com@81c103ca-a533-4d0a-5a99-9fe5e4e64535>
2011-12-01 00:52:27 +0000

    Pessoal corrigir o calculo da distancia que tava errado precisava converter para radianos A prorpiedade type esta sendo setada nula na classe player tambem corrigi Vou testar agora o comando update, mais acho que agora vai funcionar

--- trunk/Servidor/src/br/ufc/location/geoengine/GeoPositionControlTest.java
+++ trunk/Servidor/src/br/ufc/location/geoengine/GeoPositionControlTest.java
@@ -4,6 +4,7 @@ import java.util.Date;
 import java.util.List;
 
 import br.ufc.business.commands.CmdRegister;
+import br.ufc.location.facade.IMobileDevice;
 
 public class GeoPositionControlTest {
 
@@ -24,32 +25,51 @@ public class GeoPositionControlTest {
 		params[0] ="Danilo";
 		params[1] ="1";
 		params[2] ="1";
-		params[3] ="-3.717381";
-		params[4] ="38.539906";
+		params[3] ="-3.733137";
+		params[4] ="-38.497939";
 		
         cmdReg = new CmdRegister();
         resp = cmdReg.execute(params);
+        Integer myId = Integer.parseInt(resp);
         
         System.out.println("Register test: " + resp);
         
 		params[0] ="Fabio";
 		params[1] ="1";
 		params[2] ="1";
-        params[3] ="-3.717123";
-        params[4] ="38.540081";
+        params[3] ="-3.733286";
+        params[4] ="-38.497295";
 
         resp = cmdReg.execute(params);
         
         System.out.println("Register test: " + resp);
+
+		params[0] ="Viktor";
+		params[1] ="1";
+		params[2] ="2";
+        params[3] ="-3.733415";
+        params[4] ="-38.497811";
+
+        resp = cmdReg.execute(params);
         
-        latitude  = Double.parseDouble("-3.717381");
-        longitude = Double.parseDouble("38.539986");
+        System.out.println("Register test: " + resp);
+        Integer inimyId = Integer.parseInt(resp);
+        
+        latitude  = Double.parseDouble("-3.733415");
+        longitude = Double.parseDouble("-38.497810");
         Date now  = new Date();
         position  = new GeoPosition(now, latitude, longitude);
         control   = DevicesPositionControl.getInstance();
         control.updateDevicePosition(new Integer(1), position);
         
-        List list = control.getDevicesByGroup(1);
+        List<IMobileDevice> list = control.getDevicesByGroup(1);
+        
+        // Busca todos inimigos
+        List<IMobileDevice> inimys = control.getDevicesByGroup(2);
+        // busca o dispositivo pelo Id
+        IMobileDevice device= control.searchMobileDeviceById(myId);
+        // volta a lista dos inimigos visiveis
+        list = control.getVisibleDeviceList(device, inimys);
 	}
 
 }
